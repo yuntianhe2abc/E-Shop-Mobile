1f4bfb9f6a06673e753aa094071b0117d7754def
Yansong Li <liyansong2abc@gmail.com>
2024-07-19 11:43:34 +1000

    frontend expo init

---
+++ frontend/screens/Products/ProductContainer.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+
+const ProductContainer = () => {
+  return (
+    <View>
+      <Text>ProductContainer</Text>
+    </View>
+  );
+};
+
+export default ProductContainer;
